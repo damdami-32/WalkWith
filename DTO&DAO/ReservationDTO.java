d74e8b8ce6e1984740caafd1d878b6fafb524338
dkwkckzk <89624950+dkwkckzk@users.noreply.github.com>
2023-11-21 23:20:08 +0900

    Update ReservationDTO.java

--- DTO&DAO/ReservationDTO.java
+++ DTO&DAO/ReservationDTO.java
@@ -1,19 +1,22 @@
 package lab4;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class ReservationDTO {
     private int reservationId;
-    private Date resDaTi;
+    private LocalDate resDaTi;  // Date 대신 LocalDate 사용
     private String userId;
+    private String uName;
     private int storeId;
+    private String sName;
+    private String comment;
 
     // 기본 생성자
     public ReservationDTO() {
     }
 
     // 모든 필드를 초기화하는 생성자
-    public ReservationDTO(int reservationId, Date resDaTi, String userId, int storeId) {
+    public ReservationDTO(int reservationId, LocalDate resDaTi, String userId, int storeId) {
         this.reservationId = reservationId;
         this.resDaTi = resDaTi;
         this.userId = userId;
@@ -29,11 +32,11 @@ public class ReservationDTO {
         this.reservationId = reservationId;
     }
 
-    public Date getResDaTi() {
+    public LocalDate getResDaTi() {
         return resDaTi;
     }
 
-    public void setResDaTi(Date resDaTi) {
+    public void setResDaTi(LocalDate resDaTi) {
         this.resDaTi = resDaTi;
     }
 
@@ -44,20 +47,44 @@ public class ReservationDTO {
     public void setUserId(String userId) {
         this.userId = userId;
     }
+    
+    public String getuName() {
+        return uName;
+    }
 
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+    
     public int getStoreId() {
         return storeId;
     }
 
     public void setStoreId(int storeId) {
         this.storeId = storeId;
+    }	
+    
+    public String getsName() {
+        return sName;
     }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    // 코멘트 부분 추가 여기에 무슨 내용을 넣을지는 자유
+    public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+    
 
     @Override
     public String toString() {
         return "ReservationDTO [reservationId=" + reservationId + ", resDaTi=" + resDaTi + ", userId=" + userId
-                + ", storeId=" + storeId + "]";
+                + ", storeId=" + storeId + ",comment= "+ comment + "]";
     }
-    
-
 }
